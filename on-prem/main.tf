6071890f4aff1bb62cdac3846004d1cc9773785d
ORNG1 <vswsh@pm.me>
2025-05-31 15:42:57 +0200

    add for vmware provision
    Added on-prem folder with files to deploy to VMware on-premise infrastructure

---
+++ on-prem/main.tf
@@ -0,0 +1,76 @@
+ provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "win2025_vm" {
+  count            = 2
+  name             = "win2025-vm-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 16384
+  guest_id = "windows9Server64Guest" # Type générique pour Windows Server 2022+, à adapter si besoin
+
+  scsi_type = "lsilogic"
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 60 # Taille disque en Go (modifiable selon ton besoin)
+    thin_provisioned = true
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = "iso/Windows_Server_2025.iso" # chemin relatif dans le datastore
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      windows_options {
+        computer_name  = "win2025-${count.index + 1}"
+        admin_password = var.admin_password
+      }
+
+      network_interface {}
+      ipv4_gateway = var.gateway
+    }
+  }
+
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_ip_timeout  = 0
+}
---
+++ on-prem/variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+
+variable "datacenter" {}
+variable "datastore_name" {}
+variable "cluster_name" {}
+variable "network_name" {}
+variable "template_name" {} # Template Windows avec VMware Tools
+variable "admin_password" {}
+variable "gateway" {}
